bda4b0b44aacdfd5a805bf59c103d98b55e18771
Brock Alberry <vowywowy@gmail.com>
2016-12-24 18:24:26 -0500

    init
    gottem. readme soon

---
+++ app.js
@@ -0,0 +1,69 @@
+//express and socket.io
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//setting static asset path
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+//array to hold connected users
+var users = [];
+io.on('connection', (socket) => {
+	//send the client it's own id
+	socket.send(socket.id);
+	var rename = socket.id;
+
+	//add it to the array, rename will hold the user's desired name
+	users.push({
+		id: socket.id,
+		name: rename
+	});
+	//send the new user list
+	io.emit('list', users);
+
+	//on rename event
+	socket.on('name', (name) => {
+		//verify it; 1-16 characters
+		if (typeof name !== "undefined") {
+			rename = name.substr(0, 16);
+			//find that users id in the list
+			var obj = users.find(x => x.id === socket.id),
+				index = users.indexOf(obj);
+			//modify the list with the new name
+			users.fill(obj.name = rename, index, index++);
+			//send that new user list
+			io.emit('list', users);
+		}
+	});
+
+	//when we receive a chat message
+	socket.on('chat', (chat) => {
+		//verify it
+		if (typeof chat !== "undefined" && /\S/.test(chat)) {
+			//send it to everyone
+			io.emit('chat', chat.trim(), rename, socket.id);
+		}
+	});
+
+	//when a user leaves
+	socket.on('disconnect', () => {
+		var obj = users.find(x => x.id === socket.id),
+			index = users.indexOf(obj);
+		//splice the list where the user was found
+		if (index > -1) {
+			users.splice(index, 1);
+		}
+		//send that new list
+		io.emit('list', users);
+	});
+});
+
+//we listen on 80
+http.listen(port = 80, () => {
+	console.log('listening on *:' + port);
+});
---
+++ public/js/script.js
@@ -0,0 +1,88 @@
+document.addEventListener('DOMContentLoaded', () => {
+	var socket = io(),
+		myid;
+
+	//get the id of this client's socket
+	socket.on('message', (id) => {
+		myid = id;
+	});
+
+	//when we receive the list of users...
+	socket.on('list', (users) => {
+		var current = document.getElementById("users");
+		//remove the outdated list
+		while (current.firstChild) {
+			current.removeChild(current.firstChild);
+		}
+		//append the new list
+		users.forEach((user) => {
+			var add = document.createElement('div');
+			add.setAttribute('class', 'user')
+			add.textContent = user.name;
+			current.appendChild(add);
+		});
+		document.getElementById("title").textContent = 'Users Connected (' + users.length + ')'
+	});
+
+	//listen for events on #chat and #name
+	['chat', 'name'].forEach(i =>
+		document.getElementById(i).addEventListener('keydown', (e) => {
+			//if key is enter and not shift or it's enter in #name...
+			if ((e.keyCode == 13 && !e.shiftKey) || (e.keyCode == 13 && e.target.id == 'name')) {
+				e.preventDefault();
+				var input = document.getElementById(i);
+				if (e.target.id == 'name'){
+					input.setAttribute('placeholder','Typing as ' + input.textContent + " (click to change)")
+				}
+				//string testing, of course this is mirrored on the server
+				if (/\S/.test(input.innerText)) {
+					socket.emit(e.target.id, input.innerText.trim());
+					input.innerHTML = '';
+				}
+				document.getElementById('chat').focus();
+			}
+		})
+	);
+
+	var nameInput = document.getElementById('name');
+	//listen for focus and blur on this ^
+	['focus', 'blur'].forEach(e =>
+		nameInput.addEventListener(e, (e) => {
+			if (e.type == 'focus') {
+				nameInput.style.padding = "16px";
+			} else {
+				nameInput.style.padding = "4px";
+			}
+		})
+	);
+
+	//when we receive a new message...
+	socket.on('chat', (text, user, id) => {
+		//create the element appropriately
+		var chat = document.createElement('div'),
+			userSpan = document.createElement('span'),
+			textSpan = document.createElement('span'),
+			container = document.getElementById('container'),
+			box = document.getElementById('box');
+		userSpan.setAttribute('class', 'username');
+		textSpan.setAttribute('class', 'text');
+		userSpan.textContent = user + ": ";
+		textSpan.innerText = text;
+
+		//check if it's from yourself
+		if (id === myid) {
+			chat.setAttribute('class', 'chat me');
+		} else {
+			chat.setAttribute('class', 'chat other');
+		}
+
+		//append it
+		chat.appendChild(userSpan);
+		chat.appendChild(textSpan);
+		container.appendChild(chat);
+		box.scrollTop = box.scrollHeight;
+	});
+
+	//always refocus the chatinput
+	document.getElementById('name').focus();
+});
